58a9adb9088a8d6bf2538e613b22fa04de6aeebe
Andrei Pavlenko <pavlenkoandrei97@gmail.com>
2020-02-17 11:51:55 +0200

    Configure build

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+const { watch, dest } = require('gulp');
+const browserify = require('browserify');
+const source = require('vinyl-source-stream');
+
+const b = browserify('./index.js');
+
+function bundle() {
+  return b.bundle()
+    .pipe(source('bundle.js'))
+    .pipe(dest('./'));
+}
+
+function watchFiles() {
+  watch('./index.js', { ignoreInitial: false }, bundle);
+}
+
+exports.default = watchFiles;
+exports.bundle = bundle;
