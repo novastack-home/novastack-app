74f6c7e5573152dfc1f016391c38a6a15e38cfbb
Andrei Pavlenko <pavlenkoandrei97@gmail.com>
2020-07-24 10:15:52 +0300

    Change progress overlay look

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
 import LinearProgress from '@material-ui/core/LinearProgress';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
@@ -12,7 +11,7 @@ function LinearProgressWithLabel(props) {
         <LinearProgress variant="determinate" {...props} />
       </Box>
       <Box minWidth={35}>
-        <Typography variant="body2" color="textSecondary">{`${Math.round(
+        <Typography variant="body2">{`${Math.round(
           props.value,
         )}%`}</Typography>
       </Box>
