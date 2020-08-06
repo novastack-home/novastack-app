862871ae08dcafb85955671ce1231d7848b69167
Andrei Pavlenko <pavlenkoandrei97@gmail.com>
2020-08-06 14:41:34 +0300

    Change model for test

--- src/models-config.js
+++ src/models-config.js
@@ -1,7 +1,7 @@
 export const modelsConfig = [
   {id: 0, name: "Model 1", path : "models/m1/scene.gltf", position : [0.0, -0.5, 0.0], rotation : [0.0, 0.0, 0.0], scale : 0.2},
   {id: 1, name: "Model 2", path : "models/m2/scene.gltf", position : [0.0, -1.0, 0.0], rotation : [0.0, 1.0, 0.0], scale : 0.005},
-  {id: 2, name: "Model 3", path : "models/m3/scene.gltf", position : [0.0, 0.0, 0.0], rotation : [0.0, 0.0, 0.0], scale : 0.1},
+  {id: 2, name: "Model 3", path : "models/dancing/scene.gltf", position : [0.0, -1.0, 0.0], rotation : [0.0, 0.0, 0.0], scale : 1.0},
   {id: 6, name: "Model 4", path : "models/m4/scene.gltf", position : [0.0, -0.7, 0.0], rotation : [0.0, Math.PI, 0.0], scale : 1.0},
   {id: 4, name: "Model 5", path : "models/m5/scene.gltf", position : [0.0, -0.5, 0.0], rotation : [0.0, 0.0, 0.0], scale : 0.05},
   {id: 5, name: "Model 6", path : "models/m6/scene.gltf", position : [0.0, 0.0, 0.0], rotation : [0.0, 0.0, 0.0], scale : 0.4}
