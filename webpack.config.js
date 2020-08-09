f10b64b67bedafe1155b4afa16ff85c24b186be6
Andrew Goupil <andrewc.goupil@gmail.com>
2020-08-08 20:38:03 -0400

    Removing old dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -10,10 +10,6 @@ module.exports = {
     module: {
         rules: [
             { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
-            {
-                test: /\.scss$/,
-                use: ["style-loader", "css-loader", "sass-loader"],
-            },
         ],
     },
 };
