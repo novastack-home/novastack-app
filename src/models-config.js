e543f8f2c46d0de1b0d79159b5c0b3f0e4457929
Jbabs_cg <design@novastack.io>
2020-12-24 17:02:57 -0500

    styling-12-24-i

--- src/models-config.js
+++ src/models-config.js
@@ -19,8 +19,8 @@ const boomBoxBoy = {
 
 const modelsConfig = [
   { id: 0, name: 'Model 1', path: 'models/m1/scene.gltf', position: [-1.4, -0.5, -1.0], rotation: [0.0, 0.0, 0.0], scale: 0.2 },
-  { id: 1, name: 'Model 2', path: 'models/m2/scene.gltf', position: [0.0, -0.1, 0.0], rotation: [0.0, -2.3, 0.0], scale: 0.001 },
-  { id: 2, name: 'Model 3', path: 'models/m3/scene.gltf', position: [-1.5, -0.7, -0.5], rotation: [0.0, -1.5, 0.0], scale: 0.14 },
+  { id: 1, name: 'Model 2', path: 'models/m2/scene.gltf', position: [0.0, -0.1, 0.0], rotation: [0.0, -2.5, 0.0], scale: 0.001 },
+  { id: 2, name: 'Model 3', path: 'models/m3/scene.gltf', position: [-1.5, -0.5, -0.5], rotation: [0.0, -1.5, 0.0], scale: 0.12 },
   boomBoxBoy,
   { id: 4, name: 'Model 5', path: 'models/m5/scene.gltf', position: [0.0, -0.5, 0.0], rotation: [0.0, 0.0, 2.0], scale: 0.05 },
   { id: 5, name: 'Model 6', path: 'models/m6/scene.gltf', position: [0.0, 0.0, 0.0], rotation: [0.0, 0.7, 0.0], scale: 0.28 },
